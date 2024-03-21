5076b39e694b513462302272c80ecffffc23683e
Pedro Henrique <pedro.alves@germinare.org.br>
2024-03-21 06:35:02 -0300

    Atualizando arquivos Souls (Pedro Henrique)

---
+++ Dark_souls.py
@@ -0,0 +1,16 @@
+from Objetos import *
+
+nome_jogador = input("\nDigite o nome do seu personagem: ")
+jogador_principal = Caracter(nome_jogador,100, 100, 20, 20, 10, 0, 2,2)
+
+inimigo_1 = Inimigo("Inimigo 1", 50, 15, 5, 1,5, 10)
+inimigo_2 = Inimigo("Inimigo 2", 60, 18, 8, 1,5, 10)
+inimigo_3 = Inimigo("Inimigo 3", 70, 22, 10,1,5, 10)
+
+print("\nBem-vindo ao Jogo de Aventura!")
+print(f"{jogador_principal.name}, prepare-se para a batalha!\n")
+
+batalha(jogador_principal, inimigo_1)
+batalha(jogador_principal, inimigo_2)
+batalha(jogador_principal, inimigo_3)
+print('')
---
+++ Equipament_inimigos/__init__.py
@@ -0,0 +1,12 @@
+from Objetos import *
+from random import randint
+
+# Ordem de organizar Personagem 
+# --  name, hpi (Cópia hp), hp, atk, mgk, df, ouro, php = 0 (), pmk = 0,lv =0, xp =0, m= 0
+
+# Ordem de organizar inimigos
+# --  name, hp, atk, df,lv,ouro, xp
+Rato_Gigante = Inimigo('Rato Gigante', 12, 11,3, 1,5, xp = randint(3,4))
+Slime = Inimigo('Slime',15,12,4,1,5, xp = randint(3,6))
+Morcego = Inimigo('Morcego',13,11,3,1,5, xp=randint(2,4))
+Águia = Inimigo('Águia',18,15,5,1,5, xp = randint(10,11))
