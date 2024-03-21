573f5c7d3da312add5f9bc046113e2f43f73a8ba
Pedro Henrique <pedro.alves@germinare.org.br>
2024-03-21 06:52:46 -0300

    (2)Atuliazando arquivos (Pedro Henrique)

--- Dark_souls.py
+++ Dark_souls.py
@@ -1,5 +1,5 @@
 from Objetos import *
-
+from Inimigos import *
 nome_jogador = input("\nDigite o nome do seu personagem: ")
 jogador_principal = Caracter(nome_jogador,100, 100, 20, 20, 10, 0, 2,2)
 
@@ -15,8 +15,3 @@ batalha(jogador_principal, inimigo_2)
 batalha(jogador_principal, inimigo_3)
 print('')
 print('XP total obtido: ',jogador_principal.xp)
-
-Rato_Gigante = Inimigo('Rato Gigante', 12, 11,3, 1,5, xp = randint(3,4))
-Slime = Inimigo('Slime',15,12,4,1,5, xp = randint(3,6))
-Morcego = Inimigo('Morcego',13,11,3,1,5, xp=randint(2,4))
-Águia = Inimigo('Águia')
